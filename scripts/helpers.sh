54a644a446aa7ec7a6322661c8f274b792242e0e
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2025-06-24 14:53:02 -0700

    Added newlines to printfs

--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -34,15 +34,16 @@ function success() {
 
 function report() {
   # Output supplied line of text in a distinctive color.
-  printf "%b%s%b" "$COLOR_REPORT" "$1" "$COLOR_RESET"
+  printf "%b%s%b\n" "$COLOR_REPORT" "$1" "$COLOR_RESET"
 }
 
 function adjust_setting() {
   # Output supplied line of text in a distinctive color, prefaced by "$SYMBOL_ADJUST_SETTING.
+  # It is intentional to NOT have a newline. This will be supplied by success().
   printf "%b%s%s%b" "$COLOR_ADJUST_SETTING" "$SYMBOL_ADJUST_SETTING" "$1" "$COLOR_RESET"
 }
 
 function action_taken() {
   # Output supplied line of text in a distinctive color, prefaced by "$SYMBOL_ADJUST_SETTING.
-  printf "%b%s%s%b" "$COLOR_ACTION_TAKEN" "$SYMBOL_ACTION_TAKEN" "$1" "$COLOR_RESET"
+  printf "%b%s%s%b\n" "$COLOR_ACTION_TAKEN" "$SYMBOL_ACTION_TAKEN" "$1" "$COLOR_RESET"
 }
