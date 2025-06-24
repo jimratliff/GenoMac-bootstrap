35306a3eb55d0826c0596d02d2817ee50601c5f7
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2025-06-24 14:42:36 -0700

    Create helpers.sh

---
+++ scripts/helpers.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env zsh
+# Set up and assign colors
+ESC_SEQ="\033["
+
+COLOR_RESET="${ESC_SEQ}0m"
+
+COLOR_BLACK="${ESC_SEQ}30;01m"
+COLOR_RED="${ESC_SEQ}31;01m"
+COLOR_GREEN="${ESC_SEQ}32;01m"
+COLOR_YELLOW="${ESC_SEQ}33;01m"
+COLOR_BLUE="${ESC_SEQ}34;01m"
+COLOR_MAGENTA="${ESC_SEQ}35;01m"
+COLOR_CYAN="${ESC_SEQ}36;01m"
+COLOR_WHITE="${ESC_SEQ}37;01m"
+
+COLOR_REPORT="$COLOR_BLUE"
+COLOR_ADJUST_SETTING="$COLOR_CYAN"
+COLOR_ACTION_TAKEN="$COLOR_GREEN"
+COLOR_WARNING="$COLOR_YELLOW"
+
+SYMBOL_OK="✅ "
+SYMBOL_ADJUST_SETTING="⚙️ "
+SYMBOL_ACTION_TAKEN="🪚 "
+SYMBOL_WARNING="🚨 "
+
+# Example usage
+# Each %b and %s maps to a successive argument to printf
+# printf "%b[ok]%b %s\n" "$COLOR_GREEN" "$COLOR_RESET" "some message"
+
+function ok() {
+  # Terminates a line of output with the OK symbol ($SYMBOL_OK)
+  printf "${SYMBOL_OK}\n"
+}
+
+function report() {
+  # Output supplied line of text in a distinctive color.
+  printf "%b%s%b" "$COLOR_REPORT" "$1" "$COLOR_RESET"
+}
+
+function adjust_setting() {
+  # Output supplied line of text in a distinctive color, prefaced by "$SYMBOL_ADJUST_SETTING.
+  printf "%b%s%s%b" "$COLOR_ADJUST_SETTING" "$SYMBOL_ADJUST_SETTING" "$1" "$COLOR_RESET"
+}
+
+function action_taken() {
+  # Output supplied line of text in a distinctive color, prefaced by "$SYMBOL_ADJUST_SETTING.
+  printf "%b%s%s%b" "$COLOR_ACTION_TAKEN" "$SYMBOL_ACTION_TAKEN" "$1" "$COLOR_RESET"
+}
