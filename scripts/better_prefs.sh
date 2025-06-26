7b0ac4b1bcfcf149934a83c3dabca190eaf4d9fb
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2025-06-25 18:57:34 -0700

    Update better_prefs.sh

--- scripts/better_prefs.sh
+++ scripts/better_prefs.sh
@@ -135,8 +135,8 @@ defaults write com.apple.DiskUtility SidebarShowAllDevices -bool true;success_or
 ############### Kill each affected app
 action_taken "Force quit all apps/processes whose settings we just changed"
 for app in "Finder" "SystemUIServer" "Dock" "cfprefsd" "yabadaba"; do
-#  killall "${app}" && printf "Killed app: %s" "$app";success_or_not
-  killall "${app}" ; killed_app "$app";success_or_not
+  killed_app "$app"
+  killall "${app}";success_or_not
 done
 
 report "It’s possible that some settings won’t take effect until after you logout or restart."
