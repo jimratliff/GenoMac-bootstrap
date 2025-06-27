6a13c8724e7da5403e9bf7c06e7fa3d3f377faaf
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2025-06-27 15:44:55 -0700

    Add app-state persistence

--- scripts/better_prefs.sh
+++ scripts/better_prefs.sh
@@ -57,6 +57,18 @@ defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true;succe
 
 #↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑ END section ONLY for VANILLA/CONFIGURER accounts
 
+############### Enable app-state persistence
+report_action_taken "Implement app-state persistence"
+
+report_adjust_setting "1 of 3: loginwindow: TALLogoutSavesState: true"
+defaults write com.apple.loginwindow TALLogoutSavesState -bool true;success_or_not
+
+report_adjust_setting "2 of 3: loginwindow: LoginwindowLaunchesRelaunchApps: true"
+defaults write com.apple.loginwindow LoginwindowLaunchesRelaunchApps -bool true;success_or_not
+
+report_adjust_setting "3 of 3: NSGlobalDomain: NSQuitAlwaysKeepsWindows: true"
+defaults write NSGlobalDomain NSQuitAlwaysKeepsWindows -bool true;success_or_not
+
 ############### General interface
 report_action_taken "Change settings related to the user interface in general"
 
