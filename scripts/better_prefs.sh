c65ab5c1c2f51751b36d70dd5300890d892933f7
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2025-06-28 16:47:36 -0700

    Update better_prefs.sh

--- scripts/better_prefs.sh
+++ scripts/better_prefs.sh
@@ -263,9 +263,9 @@ defaults write com.apple.DiskUtility SidebarShowAllDevices -bool true;success_or
 
 ############### Terminal
 report_action_taken "Give the Terminal a teeny bit of style, even though we will soon abandon it"
-report_adjust_setting "Terminal: default for new windows: “Man Page”"
+report_adjust_setting "Terminal: default for new windows: “Man Page”";success_or_not
 defaults write com.apple.Terminal "Default Window Settings" -string "Man Page"
-report_adjust_setting "Terminal: default for starting windows: “Man Page”"
+report_adjust_setting "Terminal: default for starting windows: “Man Page”";success_or_not
 defaults write com.apple.Terminal "Startup Window Settings" -string "Man Page"
 
 ############### Kill each affected app
