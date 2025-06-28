6846815b2692ad62fcf923a06901a4aa7d0baf02
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2025-06-27 20:15:36 -0700

    Update better_prefs.sh

--- scripts/better_prefs.sh
+++ scripts/better_prefs.sh
@@ -164,16 +164,22 @@ defaults write com.apple.controlcenter "NSStatusItem Visible Bluetooth" -bool tr
 ############### Dock
 report_action_taken "Implement Dock settings"
 
-report_adjust_setting "Turn on magnification effect when hovering over Dock"
+report_adjust_setting "Dock: Turn on magnification effect when hovering over Dock"
 defaults write com.apple.dock magnification -bool true;success_or_not
 
-report_adjust_setting "Set size of magnified Dock icons"
+report_adjust_setting "Dock: Set size of magnified Dock icons"
 defaults write com.apple.dock largesize -float 128;success_or_not
 
-report_adjust_setting "Turn OFF automatic hide/show the Dock"
+report_adjust_setting "Dock: Show indicator lights for open apps"
+defaults write com.apple.dock show-process-indicators -bool true
+
+report_adjust_setting "Make Dock icons of hidden apps translucent"
+defaults write com.apple.Dock showhidden -bool true
+
+report_adjust_setting "Dock: Turn OFF automatic hide/show the Dock"
 defaults write com.apple.dock autohide -bool false;success_or_not
 
-report_adjust_setting "Enable two-finger scrolling on Dock icon to reveal thumbnails of all windows for that app"
+report_adjust_setting "Dock: Enable two-finger scrolling on Dock icon to reveal thumbnails of all windows for that app"
 defaults write com.apple.dock scroll-to-open -bool true;success_or_not
 
 ############### Finder
