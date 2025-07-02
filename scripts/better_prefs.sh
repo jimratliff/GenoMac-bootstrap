06f2401cdc7d36faa6ec9a04e3660ab86c257193
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2025-07-02 16:31:08 -0700

    Update better_prefs.sh

--- scripts/better_prefs.sh
+++ scripts/better_prefs.sh
@@ -323,6 +323,9 @@ defaults write com.apple.controlcenter "NSStatusItem Visible Bluetooth" -bool tr
 ############### Dock
 report_action_taken "Implement Dock settings"
 
+report_adjust_setting "Dock: Turn OFF automatic hide/show the Dock"
+defaults write com.apple.dock autohide -bool false;success_or_not
+
 report_adjust_setting "Dock: Turn on magnification effect when hovering over Dock"
 defaults write com.apple.dock magnification -bool true;success_or_not
 
@@ -332,21 +335,18 @@ defaults write com.apple.dock largesize -float 128;success_or_not
 report_adjust_setting "Dock: Show indicator lights for open apps"
 defaults write com.apple.dock show-process-indicators -bool true;success_or_not
 
-# This isn’t working for me (6/27/2025 on M1 Mac mini)
 report_adjust_setting "Make Dock icons of hidden apps translucent"
 defaults write com.apple.Dock showhidden -bool true;success_or_not
-
-report_adjust_setting "Highlight the element of a grid-view Dock stack over which the cursor hovers"
-defaults write com.apple.dock mouse-over-hilte-stack -bool true;success_or_not
-
-report_adjust_setting "Dock: Turn OFF automatic hide/show the Dock"
-defaults write com.apple.dock autohide -bool false;success_or_not
 
 report_adjust_setting "Dock: Enable two-finger scrolling on Dock icon to reveal thumbnails of all windows for that app"
 defaults write com.apple.dock scroll-to-open -bool true;success_or_not
 
-report_adjust_setting "Disable the Launchpad gesture (pinch with thumb and three fingers)"
-defaults write com.apple.dock showLaunchpadGestureEnabled -int 0;success_or_not
+report_adjust_setting "Minimize app to Dock rather than to app’s Dock icon"
+defaults write com.apple.dock minimize-to-application -bool false;success_or_not
+
+# This is NOT working as of 7/2/2025
+report_adjust_setting "Highlight the element of a grid-view Dock stack over which the cursor hovers"
+defaults write com.apple.dock mouse-over-hilte-stack -bool true;success_or_not
 
 ############### Mission Control/Spaces
 report_action_taken "Implement settings related to Spaces (Mission Control)"
