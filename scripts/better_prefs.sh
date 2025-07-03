73c82d93b5079bca1308a3a94e216e1d80e0274e
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2025-07-02 21:26:27 -0700

    Update better_prefs.sh

--- scripts/better_prefs.sh
+++ scripts/better_prefs.sh
@@ -362,23 +362,26 @@ defaults write com.apple.dock mouse-over-hilte-stack -bool true;success_or_not
 report_action_taken "Implement settings related to Screen Capture"
 
 path_for_screen_capture_result="$HOME/Screenshots"
-report_adjust_setting "1 of n: Create screen-capture destination directory if necessary"
+report_adjust_setting "1 of 7: Create screen-capture destination directory if necessary"
 mkdir -p "$path_for_screen_capture_result";success_or_not
 
-report_adjust_setting "2 of n: Enforce appropriate permissions (700) on screen-capture directory"
+report_adjust_setting "2 of 7: Enforce appropriate permissions (700) on screen-capture directory"
 chmod 700 "$path_for_screen_capture_result";success_or_not
 
-report_adjust_setting "3 of n: Assign path to screen-capture destination"
+report_adjust_setting "3 of 7: Assign path to screen-capture destination"
 defaults write com.apple.screencapture location -string "$path_for_screen_capture_result";success_or_not
-report_adjust_setting "4 of n: Assign path to previous screen-capture destination"
+report_adjust_setting "4 of 7: Assign path to previous screen-capture destination"
 defaults write com.apple.screencapture location-last -string "$path_for_screen_capture_result";success_or_not
 
-report_adjust_setting "5 of n: Specify that target is a file"
+report_adjust_setting "5 of 7: Specify that target is a file"
 # Needed only when overriding an assignment to clipboard or an app (e.g., Mail, Preview)
 defaults write com.apple.screencapture target -string "file";success_or_not
 
-report_adjust_setting "6 of n: Show floating thumbnail (reinforces default)"
+report_adjust_setting "6 of 7: Show floating thumbnail (reinforces default)"
 defaults write com.apple.screencapture show-thumbnail -bool true;success_or_not
+
+report_adjust_setting "7 of 7: Disable the drop shadow on screenshots"
+defaults write com.apple.screencapture disable-shadow -bool "false";success_or_not
 
 ############### Mission Control/Spaces
 report_action_taken "Implement settings related to Spaces (Mission Control)"
