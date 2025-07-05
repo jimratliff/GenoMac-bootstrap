a00abdb46611599d00aea27f09cde24613d6bac1
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2025-07-04 17:09:36 -0700

    Update better_prefs.sh

--- scripts/better_prefs.sh
+++ scripts/better_prefs.sh
@@ -302,18 +302,26 @@ defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false;s
 report_adjust_setting "2 of 2: WebAutomaticSpellingCorrectionEnabled";success_or_not
 defaults write NSGlobalDomain WebAutomaticSpellingCorrectionEnabled -bool false;success_or_not
 
-
-
-report_adjust_setting "1 of 6: Turn off automatic capitalization"
+report_adjust_setting "Turn OFF: Capitalize words automatically"
 defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false;success_or_not
+
+# Turning off inline predictive text not currently chosen to be implemented, but could be:
+# report_adjust_setting "Turn OFF: Show inline predictive text"
+# defaults write NSGlobalDomain NSAutomaticInlinePredictionEnabled -bool false;success_or_not
+
+# INTERESTING: Two different Macs (both running Sequoia macOS 15.5) disagree whether there is now a
+# “Show suggested replies” option. My Mac Studio has it (which has been upgraded over time); 
+# my pristine M1 Mac mini does not have it.
+
+report_adjust_setting "Turn OFF: Add period with double-space"
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false;success_or_not
+
+report_action_taken "Turn OFF: Use smart quotes and dashes"
+
 report_adjust_setting "2 of 6: Don’t automatically substitute dash/hyphen types"
 defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false;success_or_not
-report_adjust_setting "3 of 6: Don’t automatically substitute periods"
-defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false;success_or_not
 report_adjust_setting "4 of 6: I’ll supply the intelligence for my quotation marks, thank you!"
 defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false;success_or_not
-report_adjust_setting "6 of 6: Don’t replace, in a web context, my properly considered spelling with your arrogant replacements"
-defaults write NSGlobalDomain WebAutomaticSpellingCorrectionEnabled -bool false;success_or_not
 
 ############### Keyboard-related defaults
 report_action_taken "Implement keyboard-related defaults"
