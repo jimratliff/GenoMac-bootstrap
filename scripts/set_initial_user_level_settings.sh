4adc25b757cb2fbc84a2d2c0ae38e17b0cecba4b
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2025-07-04 21:42:31 -0700

    Update set_initial_user_level_settings.sh

--- scripts/set_initial_user_level_settings.sh
+++ scripts/set_initial_user_level_settings.sh
@@ -127,32 +127,34 @@ defaults write com.apple.dock minimize-to-application -bool false;success_or_not
 # defaults write com.apple.dock mouse-over-hilte-stack -bool true;success_or_not
 
 ############### Screen Capture
-report_action_taken "Implement settings related to Screen Capture"
+set_screen_capture_settings
 
-# NOTE: Setting the location should be separated from the other screen-capture preferences because this
-#       would be user-specific
-path_for_screen_capture_result="$HOME/Screenshots"
-report_adjust_setting "1 of 7: Create screen-capture destination directory if necessary"
-mkdir -p "$path_for_screen_capture_result";success_or_not
-
-report_adjust_setting "2 of 7: Enforce appropriate permissions (700) on screen-capture directory"
-# The 700 permissions are appropriate because this location in a user’s home directory.
-chmod 700 "$path_for_screen_capture_result";success_or_not
-
-report_adjust_setting "3 of 7: Assign path to screen-capture destination"
-defaults write com.apple.screencapture location -string "$path_for_screen_capture_result";success_or_not
-report_adjust_setting "4 of 7: Assign path to previous screen-capture destination"
-defaults write com.apple.screencapture location-last -string "$path_for_screen_capture_result";success_or_not
-
-report_adjust_setting "5 of 7: Specify that target is a file"
-# Needed only when overriding an assignment to clipboard or an app (e.g., Mail, Preview)
-defaults write com.apple.screencapture target -string "file";success_or_not
-
-report_adjust_setting "6 of 7: Show floating thumbnail (reinforces default)"
-defaults write com.apple.screencapture show-thumbnail -bool true;success_or_not
-
-report_adjust_setting "7 of 7: Disable the drop shadow on screenshots"
-defaults write com.apple.screencapture disable-shadow -bool "true";success_or_not
+# report_action_taken "Implement settings related to Screen Capture"
+# 
+# # NOTE: Setting the location should be separated from the other screen-capture preferences because this
+# #       would be user-specific
+# path_for_screen_capture_result="$HOME/Screenshots"
+# report_adjust_setting "1 of 7: Create screen-capture destination directory if necessary"
+# mkdir -p "$path_for_screen_capture_result";success_or_not
+# 
+# report_adjust_setting "2 of 7: Enforce appropriate permissions (700) on screen-capture directory"
+# # The 700 permissions are appropriate because this location in a user’s home directory.
+# chmod 700 "$path_for_screen_capture_result";success_or_not
+# 
+# report_adjust_setting "3 of 7: Assign path to screen-capture destination"
+# defaults write com.apple.screencapture location -string "$path_for_screen_capture_result";success_or_not
+# report_adjust_setting "4 of 7: Assign path to previous screen-capture destination"
+# defaults write com.apple.screencapture location-last -string "$path_for_screen_capture_result";success_or_not
+# 
+# report_adjust_setting "5 of 7: Specify that target is a file"
+# # Needed only when overriding an assignment to clipboard or an app (e.g., Mail, Preview)
+# defaults write com.apple.screencapture target -string "file";success_or_not
+# 
+# report_adjust_setting "6 of 7: Show floating thumbnail (reinforces default)"
+# defaults write com.apple.screencapture show-thumbnail -bool true;success_or_not
+# 
+# report_adjust_setting "7 of 7: Disable the drop shadow on screenshots"
+# defaults write com.apple.screencapture disable-shadow -bool "true";success_or_not
 
 ############### Mission Control/Spaces
 report_action_taken "Implement settings related to Spaces (Mission Control)"
