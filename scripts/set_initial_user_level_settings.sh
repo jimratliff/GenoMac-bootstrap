31b35a410c8b016c5fafde5088c348914a87c6cb
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2025-07-04 21:54:47 -0700

    Update set_initial_user_level_settings.sh

--- scripts/set_initial_user_level_settings.sh
+++ scripts/set_initial_user_level_settings.sh
@@ -170,12 +170,6 @@ defaults write com.apple.DiskUtility SidebarShowAllDevices -bool true;success_or
 ############### Terminal
 set_terminal_settings
 
-# report_action_taken "Give the Terminal a teeny bit of style, even though we will soon abandon it"
-# report_adjust_setting "Terminal: default for new windows: “Man Page”";success_or_not
-# defaults write com.apple.Terminal "Default Window Settings" -string "Man Page"
-# report_adjust_setting "Terminal: default for starting windows: “Man Page”";success_or_not
-# defaults write com.apple.Terminal "Startup Window Settings" -string "Man Page"
-
 ############### Text Edit
 report_adjust_setting "Text Edit: Make plain text the default format"
 defaults write com.apple.TextEdit RichText -bool false;success_or_not
