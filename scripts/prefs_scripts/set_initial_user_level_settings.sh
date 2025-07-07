0c44fd2dcfa41d2b9b07e063786cb0d8bf553e6b
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2025-07-07 16:49:16 -0700

    Update set_initial_user_level_settings.sh

--- scripts/prefs_scripts/set_initial_user_level_settings.sh
+++ scripts/prefs_scripts/set_initial_user_level_settings.sh
@@ -23,7 +23,7 @@ source "${BETTER_PREFS_COMPONENTS_DIR}/set_safari_settings.sh"
 
 function set_initial_user_level_settings() {
 
-report_start_phase
+report_start_phase_standard
 
 # Enable app-state persistence
 set_app_state_persistence
@@ -133,6 +133,6 @@ defaults write com.apple.TextEdit RichText -bool false;success_or_not
 # Safari
 set_safari_settings
 
-report_end_phase
+report_end_phase_standard
 
 }
