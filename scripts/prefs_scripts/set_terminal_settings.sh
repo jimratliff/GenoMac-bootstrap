a2ca8a6c14973b4e565228a73eb92f805bf0b85f
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2025-07-07 16:50:37 -0700

    Update set_terminal_settings.sh

--- scripts/prefs_scripts/set_terminal_settings.sh
+++ scripts/prefs_scripts/set_terminal_settings.sh
@@ -13,7 +13,7 @@ source "${GENOMAC_BOOTSTRAP_HELPER_DIR}/helpers.sh"
 
 function set_terminal_settings() {
 
-report_start_phase
+report_start_phase_standard
 
 report_action_taken "Give the Terminal a teeny bit of style, even though we will soon abandon it"
 report_adjust_setting "Terminal: default for new windows: “Man Page”";success_or_not
@@ -21,6 +21,6 @@ defaults write com.apple.Terminal "Default Window Settings" -string "Man Page"
 report_adjust_setting "Terminal: default for starting windows: “Man Page”";success_or_not
 defaults write com.apple.Terminal "Startup Window Settings" -string "Man Page"
 
-report_end_phase
+report_end_phase_standard
 
 }
