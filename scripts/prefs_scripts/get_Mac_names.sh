9dc7efc10e964f411547302aeeefa595d7200b8f
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2025-07-10 20:36:02 -0700

    Update get_Mac_names.sh

--- scripts/prefs_scripts/get_Mac_names.sh
+++ scripts/prefs_scripts/get_Mac_names.sh
@@ -36,8 +36,8 @@ echo "Current ComputerName: \"$current_name\""
 final_name_is_dirty=false
 
 # If current_name ends with ' (###)', clean it
-if [[ "$current_name" =~ \ \([0-9]+\)$ ]]; then
-  final_name=$(echo "$current_name" | sed 's/ ([0-9]\{1,\})$//')
+if echo "$current_name" | grep -qE ' \([0-9]+\)$'; then
+  final_name=$(echo "$current_name" | sed -E 's/ \([0-9]+\)$//')
   report_action_taken "ComputerName appears to have been auto-mangled. I have unmangled it: \"$final_name\""
   final_name_is_dirty=true
 fi
